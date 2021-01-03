02686ea33523d5fa9a66f1e12c07a9db9ccaac34
Hypervisor <hyp3rvisor@protonmail.com>
2021-01-03 06:54:44 -0700

    Update kmain.c

--- src/kmain.c
+++ src/kmain.c
@@ -17,10 +17,10 @@ void kmain(void)
     KeClearScreen(kstdout->attrib);
     KePrintK("TEST Enter keyboard input:\n");
 
-    /* char str[50]; */
-    /* KeGets(str); */
-    /* KePrintK("\n"); */
-    /* KePrintK(str); */
+    char str[50];
+    KeGets(str);
+    KePrintK("\n");
+    KePrintK(str);
         
     while(1);
 
