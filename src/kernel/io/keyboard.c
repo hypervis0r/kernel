c6e67eda199449c216229dfa2d39b42bad4ee7ad
Hypervisor <hyp3rvisor@protonmail.com>
2021-01-03 07:08:29 -0700

    Update keyboard.c

--- src/kernel/io/keyboard.c
+++ src/kernel/io/keyboard.c
@@ -2,10 +2,7 @@
 
 static unsigned char keycode = NULL;
 
-/* The following array is taken from 
-    http://www.osdever.net/bkerndev/Docs/keyboard.htm
-   All credits where due
-*/
+
 
 
 
@@ -49,54 +46,59 @@ unsigned char KeWaitForKeyPress(void)
 
 char KeGetLastKeyPressed(unsigned char src)
 {
-    /* Keyboard map, take scancode and grab the 
-     * value at the index.
-     * TODO: figure out a way to interchange this 
-     * Also TODO: NOT HAVE IT IN A FUCKING FUNCTION THIS IS 
-     * SO FUCKING STUPID */
-    BYTE keyboard_map[128] = {
-        0,  27, 
-        '1', '2', '3', '4', '5', '6', '7', '8',	'9', '0', 
-        '-', '=', '\b',	/* Backspace */
-        '\t',			/* Tab */
-        'q', 'w', 'e', 'r',	't', 'y', 'u', 'i', 'o', 'p', '[', ']', 
-        '\n',	/* Enter key */
-        0,		/* 29   - Control */
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',   
-        0,		/* Left shift */
-        '\\', 'z', 'x', 'c', 'v', 'b', 'n',	'm', ',', '.', '/',   
-        0,		/* Right shift */
-        '*',
-        0,	    /* Alt */
-        ' ',	/* Space bar */
-        0,	    /* Caps lock */
-        0,	    /* 59 - F1 key ... > */
-        0,   0,   0,   0,   0,   0,   0,   0,
-        0,	    /* < ... F10 */
-        0,	    /* 69 - Num lock*/
-        0,	    /* Scroll Lock */
-        0,	    /* Home key */
-        0,	    /* Up Arrow */
-        0,	    /* Page Up */
-        '-',
-        0,	    /* Left Arrow */
-        0,
-        0,	    /* Right Arrow */
-        '+',
-        0,	    /* 79 - End key*/
-        0,	    /* Down Arrow */
-        0,	    /* Page Down */
-        0,	    /* Insert Key */
-        0,	    /* Delete Key */
-        0,   0,   0,
-        0,	    /* F11 Key */
-        0,	/* F12 Key */
-        0,	/* All other keys are undefined */
-    };
+	/* The following array is taken from 
+	http://www.osdever.net/bkerndev/Docs/keyboard.htm
+	All credits where due
+	*/
 
-    BYTE key_pressed = keyboard_map[src];
+	/* Keyboard map, take scancode and grab the 
+	* value at the index.
+	* TODO: figure out a way to interchange this 
+	* Also TODO: NOT HAVE IT IN A FUCKING FUNCTION THIS IS 
+	* SO FUCKING STUPID */
+	BYTE keyboard_map[128] = {
+	0,  27, 
+	'1', '2', '3', '4', '5', '6', '7', '8',	'9', '0', 
+	'-', '=', '\b',	/* Backspace */
+	'\t',		/* Tab */
+	'q', 'w', 'e', 'r',	't', 'y', 'u', 'i', 'o', 'p', '[', ']', 
+	'\n',		/* Enter key */
+	0,		/* 29   - Control */
+	'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',   
+	0,		/* Left shift */
+	'\\', 'z', 'x', 'c', 'v', 'b', 'n',	'm', ',', '.', '/',   
+	0,		/* Right shift */
+	'*',
+	0,	    	/* Alt */
+	' ',		/* Space bar */
+	0,	    	/* Caps lock */
+	0,	    	/* 59 - F1 key ... > */
+	0,   0,   0,   0,   0,   0,   0,   0,
+	0,	    	/* < ... F10 */
+	0,	    	/* 69 - Num lock*/
+	0,	    	/* Scroll Lock */
+	0,	    	/* Home key */
+	0,	    	/* Up Arrow */
+	0,	    	/* Page Up */
+	'-',
+	0,	    	/* Left Arrow */
+	0,
+	0,	    	/* Right Arrow */
+	'+',
+	0,	    	/* 79 - End key*/
+	0,	    	/* Down Arrow */
+	0,	    	/* Page Down */
+	0,	    	/* Insert Key */
+	0,	    	/* Delete Key */
+	0,   0,   0,
+	0,	    	/* F11 Key */
+	0,		/* F12 Key */
+	0,		/* All other keys are undefined */
+	};
 
-    return key_pressed;
+	BYTE key_pressed = keyboard_map[src];
+
+	return key_pressed;
 }
 
 unsigned char KeGetLastKeycode(void)
